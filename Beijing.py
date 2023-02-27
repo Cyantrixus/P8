49ad2924526fbfc7dc8842701f5a4bdd559566dc
Christoffer Lind Andersen <chri692u@gmail.com>
2023-02-27 12:24:51 +0100

    update to beijing

--- Beijing.py
+++ Beijing.py
@@ -9,22 +9,9 @@ class Beijing:
         self.graph = ox.graph_from_place("Beijing, China", network_type='drive')
 
     def write(self):
-        # Create directory if it doesnt exist
+        # Create directory and save file if it doesnt exist
         if not self.path == "" and not os.path.exists(self.path):
             os.makedirs(self.path)
-    
-        # Write shape files
-        nodefile = os.path.join(self.path, "nodes.shp")
-        edgefile = os.path.join(self.path, "edges.shp")
-
-        # Convert undirected graph to gdfs and stringify non-numeric columns
-        nodes, edges = ox.utils_graph.graph_to_gdfs(self.graph)
-        nodes = ox.io._stringify_nonnumeric_cols(nodes)
-        edges = ox.io._stringify_nonnumeric_cols(edges)
-
-        # We need an unique ID for each element in the given vector
-        ids = lambda x: np.arange(1, len(x)+1, 1)
-
-        #Assign ids
-        edges["key"] = ids(edges)
-        nodes["node"] = ids(nodes)
+            ox.io.save_graph_geopackage(self.graph, self.path + "/" + self.name)
+        else:
+            print("ROAD NETWORK ALREADY SAVED")
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import os
 import time
-from processor import Processor
+from Beijing import Beijing
+from Processor import Processor
 
-path = os.path.abspath(r"./Data/taxi_log_2008_by_id")
-files = os.listdir(path)
+# Files
+dataPath = os.path.abspath(r"./Data/taxi_log_2008_by_id")
+outputPath = os.path.abspath(r"./Output")
+files = os.listdir(dataPath)
 
-t0 = time.time()
+# Create processor
+print("CREATING PROCESSOR")
+time_start = time.time()
 p = Processor()
+print("SAVING BEIJING NETWORK")
+beijing = Beijing('Beijing', outputPath)
+beijing.write()
+time_process = time.time() - time_start
+print(f"PROCESSOR READY: {time_process} SECONDS")
 
+# Read CSV files
 for file in files:
-    p.read(path + "/" + file)
+    p.read(dataPath + "/" + file)
+    break
 
-done = time.time() - t0
+# Print time log
+done = time.time() - time_process - time_start
 print(f"TIME TO READ: {done} SECONDS")
 print(f"DATA SIZE: {len(p.data)}")
