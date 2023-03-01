99a959f22da434efe24cbca902cd5a2d5376448c
Christoffer Lind Andersen <chri692u@gmail.com>
2023-03-01 22:11:35 +0100

    load/lookup + refactor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
-from Beijing import Beijing
+import Auxiliary as aux
+from Roadnet import Roadnet
 from Processor import Processor
 
 # Files
@@ -11,19 +12,25 @@ files = os.listdir(dataPath)
 # Create processor
 print("CREATING PROCESSOR")
 time_start = time.time()
-p = Processor()
-print("SAVING BEIJING NETWORK")
-beijing = Beijing('Beijing', outputPath)
-beijing.write()
+processor = Processor()
+map = Roadnet('CPH', "Copenhagen, Denmark", outputPath)
+map.write()
 time_process = time.time() - time_start
 print(f"PROCESSOR READY: {time_process} SECONDS")
 
 # Read CSV files
 for file in files:
-    p.read(dataPath + "/" + file)
+    processor.read(dataPath + "/" + file)
     break
 
 # Print time log
 done = time.time() - time_process - time_start
 print(f"TIME TO READ: {done} SECONDS")
-print(f"DATA SIZE: {len(p.data)}")
+print(f"DATA SIZE: {len(processor.data)}")
+
+# Load + lookup example
+print(f"LOADING {map.name} MAP")
+data = map.load()
+sample = data.head()
+row = map.lookup(sample, aux.idpred, 0)
+print(row)
