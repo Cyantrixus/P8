bc52bf68174876d39432966644279639aceb343b
Christoffer Lind Andersen <chri692u@gmail.com>
2023-03-01 22:11:59 +0100

    comments

--- processor.py
+++ processor.py
@@ -1,13 +1,12 @@
 import numpy as np
-#from pynapl import APL
-
 
 class Processor:
+    # Start as empty
     def __init__(self):
-        #self.apl = APL.APL()
         self.data = []
     
+    # Read a csv file and store the data
     def read(self, path):
-        # Numpy attempt
         csv = np.recfromcsv(path, delimiter=',', encoding="utf-8")
         self.data.append(csv)
+        
