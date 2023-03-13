d331d329289bd539184c3b9125af9ec37ba62813
Christoffer Lind Andersen <chri692u@gmail.com>
2023-03-13 18:34:38 +0100

    install changes

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ sudo apt-get install libboost-dev libboost-serialization-dev \
 gdal-bin libgdal-dev make cmake libbz2-dev libexpat1-dev swig
 
 ./Anaconda3-2022.10-Linux-x86_64.sh
+
+cd ~ && ln -sf /usr/lib/x86_64-linux-gnu/libstdc++.so.6 anaconda3/lib/libstdc++.so.6
